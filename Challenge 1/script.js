5058de1ef3d3c3c126cff49a4d2c629cac4c9170
Zukile Lubanga <zukile.81.lubanga@gmail.com>
2023-08-10 05:38:04 +0200

    changed code format

--- Challenge 1/script.js
+++ Challenge 1/script.js
@@ -3,10 +3,8 @@ const secondaryPhone = '0219131568'
 
 // Only change below this line
 
-const numericRegex = /^[0-9]+$/;
-
-const primaryValid = numericRegex.test(primaryPhone);
-const secondaryValid = numericRegex.test(secondaryPhone);
+const primaryValid = !isNaN(+primaryPhone);
+const secondaryValid = !isNaN(+secondaryPhone);
 
 console.log('Primary phone is valid numerical string:', primaryValid);
-console.log('Secondary phone is valid numerical string:', secondaryValid);
+console.log('Secondary phone is valid numerical string:', secondaryValid );
