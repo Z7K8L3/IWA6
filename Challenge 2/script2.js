20dd45026be8449acfb97830fdda348761d1b3fb
Zukile Lubanga <zukile.81.lubanga@gmail.com>
2023-08-11 16:53:19 +0200

    Added script tag and edited script file

--- Challenge 2/script2.js
+++ Challenge 2/script2.js
@@ -3,15 +3,15 @@ const tax = '8%';
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
-const hourOfDay = 00;
-const minuteOfDay = 00;
+const hourOfDay = 0;
+const minuteOfDay = 0;
 
 // Only change below this line
 
-if hourOfDay && (minuteOfDay !== null) && (hourOfDay == '00') && (minuteOfDay == '00') {
-	const taxAsDecimal = tax / '100'
-  const startingAfterTax = salary * '1' - taxAsDecimal
-	const balace = starting - transport - food - rent
-}
+if (hourOfDay === 0 && minuteOfDay === 0) {
+	const taxAsDecimal = parseFloat(tax) / 100; //Convert tax to decimal
+    const startingAfterTax = salary * (1 - taxAsDecimal); //Calculate salary after tax
+	const balance = startingAfterTax - transport - food - rent; //Calculate balance
 	
-console.log(balance.toFixed(3))
+	console.log(`R ${balance.toFixed(2)}`); //Print balance with formatting
+}
